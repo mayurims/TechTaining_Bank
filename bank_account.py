cc2d486c65dbed96a97150fc7516e44e2090a3a9
mayurims <mayurims@users.noreply.github.com>
2023-08-11 13:55:40 +0800

    Created Classes

--- bank_account.py
+++ bank_account.py
@@ -1,2 +1,6 @@
-class show_account:
-    print('Hello')
+class account_details:
+    #account_type = 'Savings'
+    def __init__(self, bank_id, user_name ):
+        self.bank_id = bank_id
+        self.user_name = user_name
+    
---
+++ transaction.py
@@ -0,0 +1,7 @@
+class transaction:
+    def __init__(self, bank_id, amount, action, transaction_id):
+        self.bank_id = bank_id
+        self.amount = amount
+        self.action = action
+        self.action = transaction_id
+
