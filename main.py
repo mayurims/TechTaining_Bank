f078567862a153cc6166b386a5c027f8f883d00a
Mig <sawekpunmig@gmail.com>
2023-08-14 11:39:18 +0800

    yee

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 from classes import account, customer, transaction
 import random
+from tabulate import tabulate
 
 #customer_list = []
 bank_list = []
 transactions = []
-
-bank_list.append(customer(1, 'Mayuri', 100))
-bank_list.append(customer(2, 'Mig', 100))
-
-# transactions.append(transactions(1, 100, 'withdraw'))
-
-#print(bank_list)
+trans_display = []
 
 # Check validity of Bank ID
 def check_validity(user_input):
@@ -25,7 +20,7 @@ def check_amt(amount, bank_id, action):
     if action == 'withdraw' or action == 'transfer':
         for i in range(len(bank_list)):
             if bank_id == bank_list[i].bank_id:
-                if amount < bank_list[i].balance:
+                if amount <= bank_list[i].balance:
                     return True
         return False
     else:
@@ -34,12 +29,32 @@ def check_amt(amount, bank_id, action):
 def transid_gen():
     num = 1
     if len(transactions) == 0:
+        print(1)
         return 1
     for i in range(len(transactions)):
         if num == transactions[i].transaction_id:
             num += 1
     print(num)
     return num
+
+def see_transact(bank_id):
+    counter = 0
+    for i in reversed(range(0,len(transactions))):
+        if (bank_id == transactions[i].bank_id and counter <= 10):
+            #print(transactions[i].transaction_id)
+            trans_display.append(
+                [transactions[i].date, transactions[i].transaction_id , transactions[i].bank_id, transactions[i].amount,
+                 transactions[i].action, transactions[i].balance])
+            counter += 1
+    if len(trans_display) == 0:
+        print('No previous transactions')
+    else:
+        #table
+        trans_display.insert(0,
+            ['Date', 'Transaction_ID', 'Bank_ID', 'Amount', 'Action', 'Balance']
+        )
+        print(tabulate(trans_display))
+
 
 def transact(amount, action, bank_id, target_bank_id):
     #validate here or in outer function
@@ -52,7 +67,7 @@ def transact(amount, action, bank_id, target_bank_id):
                 bank_list[i].balance -= amount
                 print('after')
                 print(bank_list[i].balance)
-                transactions.append(transaction(transid_gen(), amount, 'withdraw'))
+                transactions.append(transaction(transid_gen(), bank_list[i].bank_id, '', amount, 'withdraw', bank_list[i].balance))
 
         #transactions.append
     elif action == 'deposit':
@@ -64,6 +79,7 @@ def transact(amount, action, bank_id, target_bank_id):
                 bank_list[i].balance += amount
                 print('after')
                 print(bank_list[i].balance)
+                transactions.append(transaction(transid_gen(), bank_list[i].bank_id, '', amount, 'deposit', bank_list[i].balance))
     elif action == 'transfer':
         #transfer
         for i in range(len(bank_list)):
@@ -80,15 +96,29 @@ def transact(amount, action, bank_id, target_bank_id):
                         bank_list[j].balance += amount
                         print('receiver after:')
                         print(bank_list[j].balance)
+                        transactions.append(transaction(transid_gen(), bank_list[i].bank_id, bank_list[j].bank_id, amount, 'transfer', bank_list[i].balance))
+
     else:
         print('Invalid input')
 
-valid = False 
+# Adding the Database
+bank_list.append(customer(1, 'Mayuri', 1000))
+bank_list.append(customer(2, 'Mig', 350))
+bank_list.append(customer(3, 'Timothee', 400))
+bank_list.append(customer(4, 'Mig_the_annoying_person', 10))
+
+transactions.append(transaction(transid_gen(), 1, '', 100, 'withdraw', 100))
+transactions.append(transaction(transid_gen(), 1, '', 200, 'withdraw', 200))
+transactions.append(transaction(transid_gen(), 2, '', 50, 'deposit', 50))
+
+valid = False
+
 while valid == False:
     user_input = int(input('Please input your bank id : '))
     valid = check_validity(user_input)
-    if valid: 
-        action_input = input('What would you like to do - See Transactions / Withdraw / Deposit / Transfer Funds : ').lower() 
+    if valid:
+        #MENU
+        action_input = input('What would you like to do - See Transactions / Withdraw / Deposit / Transfer Funds / Exit : ').lower()
         if (action_input == 'withdraw' or action_input == 'deposit'):
             amount = int(input(f'Please enter the amount you want to {action_input} : '))
             # Check if there is sufficient amount
@@ -107,19 +137,18 @@ while valid == False:
                     print('Incorrect Account ID')
                     account_id = int(input('Please enter account number to transfer to : '))
                 transact(amount, action_input, user_input, account_id)
-                # if check_validity(account_id):
-                #     # Transact the amount
-                #     transact(amount, action_input, user_input, account_id)
-                # else:
-                #     print('Incorrect Account ID')
-                #     account_id = int(input('Please enter account number to transfer to : '))
             else:
                 print('You have insufficient Fund')
         elif(action_input == 'see transactions'):
-            print('See transactions')
+            see_transact(user_input)
+        elif(action_input == 'exit'):
+            print('Exit')
+            break
         else:
             print('False')
-    else: 
+        valid = False
+        user_input = ''
+    else:
     # If Bank ID is incorrect
     #if valid == False:
         print('Invalid ID, please try again later!')
